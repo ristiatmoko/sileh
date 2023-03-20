feb4afc52dae44f39a00fa72664ae4c0fc4aad91
Risti A <ristiatmoko@gmail.com>
2023-03-21 04:44:56 +0700

    model

---
+++ app/Models/M_komik.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_komik extends Model
+{
+    protected $table = 'komik';
+    protected $primaryKey = 'id_komik';
+    protected $useTimestamps = 'true';
+
+
+    public function getKomik($slug = false)
+    {
+        if ($slug == false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug' => $slug])->first();
+    }
+}
